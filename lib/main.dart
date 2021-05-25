abcf30f679f194335f0e508a28f35af1ef499faa
nathan-courtney <46496645+nathan-courtney@users.noreply.github.com>
2021-05-25 17:41:28 -0400

    quick fix

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:convert';
-import 'package:http_parser/http_parser.dart';
+import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
